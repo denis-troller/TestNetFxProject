c002e135441c13bfa11d67316ead95cd064ad119
denis.troller <denis.troller@sonarsource.com>
2024-04-12 14:31:29 +0200

    Add project files.

---
+++ TestNetFxProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestNetFxProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            Console.Out.WriteLine("Done");
+        }
+    }
+}
